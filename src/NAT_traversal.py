e5bcd4d264136d29eb27c4a81f260ad638f3e7ad
Antonio <antoniojesus184@gmail.com>
2023-11-08 13:50:30 +0100

    Modify NAT_traversal to determine the NAT type

--- src/NAT_traversal.py
+++ src/NAT_traversal.py
@@ -1,23 +1,42 @@
 import stun
-import numpy as np
 import minimal 
 
-def get_ip_info(stun_host='stun.l.google.com', source_port=4444):
-    try:
-        nat_type, external_ip, external_port = stun.get_ip_info(stun_host=stun_host, source_port=source_port)
-        return nat_type, external_ip, external_port
-    except Exception as e:
-        print(f"Error al obtener información de red: {e}")
-        return None, None, None
-
 class NAT_traversal(minimal.Minimal):
-    pass
+    endPoints = []
+
+def get_ip_info(stun_servers):
+
+    for stun_address in stun_servers:
+        host, port = stun_address.strip().split(":")
+        nat_type, external_ip, external_port = stun.get_ip_info(stun_host=host, source_port=(int)(port))            
+        endPoint = (external_ip, external_port)    
+        if (type(external_ip)==str and type(external_port)==int):
+            NAT_traversal.endPoints.append(endPoint)
+        print(f"External Port: {external_port}\n", 
+              f"External IP: {external_ip}\n", 
+              f"End-Point: {external_ip}:{external_port}\n", 
+              f"NAT type: {nat_type}\n")  
+
+    NAT_TYPE()
+
+    return nat_type, external_ip, external_port
+
+def NAT_TYPE():
+    if all(endPoint == NAT_traversal.endPoints[0] for endPoint in NAT_traversal.endPoints):
+        print("NAT abierto (Full Cone)")
+        print("Puede establecer conexion con otro dispositivo. Para ello ejecute el siguiente comando: python3 NAT-traversal -l \'puerto de escucha\' -a \'ip de destino\' -p \'puerto de destino\'")     
+    if all(endPoint != NAT_traversal.endPoints[0] for endPoint in NAT_traversal.endPoints):
+        print("NAT simetrico")
+        print("No puede establecer conexion con otro dispositivo.")
+
 if __name__ == "__main__": 
-    nat_type, external_ip, external_port = get_ip_info()
-    if nat_type is not None:
-        print(f"Tipo de NAT: {nat_type}")
-        print(f"IP externa: {external_ip}")
-        print(f"Puerto externo: {external_port}")
+    stun_servers = ["stun.l.google.com:4444", "stun1.l.google.com:19302", "stun.l.google.com:19303"]
+    nat_type, external_ip, external_port = get_ip_info(stun_servers)
     minimal.args = minimal.parser.parse_known_args()[0]
     otro = NAT_traversal()
-    otro.run()
+    try:
+        otro.run()
+    except KeyboardInterrupt:
+        minimal.parser.exit("\nSIGINT received")
+    finally:
+        otro.print_final_averages()
