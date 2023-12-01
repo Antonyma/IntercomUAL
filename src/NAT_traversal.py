10a05d6b1926f5106df4f94f7dddce96f326f0a6
Antonio <antoniojesus184@gmail.com>
2023-12-01 21:09:06 +0100

    Modify NAT_traversal

--- src/NAT_traversal.py
+++ src/NAT_traversal.py
@@ -1,33 +1,32 @@
 import stun
-import minimal 
+import minimal
 
 class NAT_traversal(minimal.Minimal):
     endPoints = []
 
 def get_ip_info(stun_servers):
-
     for stun_address in stun_servers:
         host, port = stun_address.strip().split(":")
-        nat_type, external_ip, external_port = stun.get_ip_info(stun_host=host, source_port=(int)(port))            
+        nat_type, external_ip, external_port = stun.get_ip_info(stun_host=host, source_port=int(port))            
         endPoint = (external_ip, external_port)    
-        if (type(external_ip)==str and type(external_port)==int):
+        if type(external_ip) == str and type(external_port) == int:
             NAT_traversal.endPoints.append(endPoint)
         print(f"External Port: {external_port}\n", 
               f"External IP: {external_ip}\n", 
               f"End-Point: {external_ip}:{external_port}\n", 
               f"NAT type: {nat_type}\n")  
-        
-    NAT_TYPE()
+
+    NAT_TYPE(stun_servers)
     return nat_type, external_ip, external_port
 
-def NAT_TYPE():
-    if all(endPoint == NAT_traversal.endPoints[0] for endPoint in NAT_traversal.endPoints):
-        print("Full Cone NAT)")
-    if all(endPoint != NAT_traversal.endPoints[0] for endPoint in NAT_traversal.endPoints):
-        print("Symetric NAT")
+def NAT_TYPE(stun_servers):
+    if all(endPoint == NAT_traversal.endPoints[0] for endPoint in stun_servers):
+        print("Full Cone NAT")
+    if all(endPoint != NAT_traversal.endPoints[0] for endPoint in stun_servers):
+        print("Symmetric NAT")
         print("Cannot establish connection with another device.")
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     stun_servers = ["stun.l.google.com:4444", "stun1.l.google.com:19302", "stun.l.google.com:19303"]
     nat_type, external_ip, external_port = get_ip_info(stun_servers)
     minimal.args = minimal.parser.parse_known_args()[0]
